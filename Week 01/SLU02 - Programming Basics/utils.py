d9b272d8d2406c13d72422946497dc579c9faa36
majkah0 <mariahanulova@yahoo.com>
2024-03-25 10:43:50 +0100

    slu02

---
+++ Week 01/SLU02 - Programming Basics/utils.py
@@ -0,0 +1,10 @@
+import inspect
+
+def variable_ex31_1():
+    return 'pots_of_honey'
+
+def variable_ex31_2():
+   return 'poemsByEeyore'
+
+def variable_ex31_3():    
+   return 'BalloonFromPiglet'
