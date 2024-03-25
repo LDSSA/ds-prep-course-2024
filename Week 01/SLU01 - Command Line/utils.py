515016f5bdfedba877c23413213b72b04f2d4137
majkah0 <mariahanulova@yahoo.com>
2024-03-25 10:19:30 +0100

    slu01

---
+++ Week 01/SLU01 - Command Line/utils.py
@@ -0,0 +1,2 @@
+def message_ex_10():
+    print('Yes, json files are also text files! You can try for yourself, try opening one in a text editor like a notepad.\nYou see that some files are not what they seem at first sight and that they are certainly not defined by their extension. ;)' )
